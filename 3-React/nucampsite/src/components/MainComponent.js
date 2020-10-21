918f57013e5e74807b5eb966a84b140b22a7e455
Patrick <prjr25@gmail.com>
2020-10-20 20:42:02 -0700

    React Router

--- 3-React/nucampsite/src/components/MainComponent.js
+++ 3-React/nucampsite/src/components/MainComponent.js
@@ -2,29 +2,36 @@ import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
-import CampsiteInfo from "./CampsiteInfoComponent"
 
 
 class Main extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            selectedCampsite: null
         };
     }
 
-    onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});        
-    }
-
     render() {
+
+        const HomePage = ()=>{
+            return(
+                <Home />
+            );
+        };
+
         return (
             <div>
                 <Header />
-                <Directory campsites={this.state.campsites} onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)}/>
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer />
             </div>
         );
