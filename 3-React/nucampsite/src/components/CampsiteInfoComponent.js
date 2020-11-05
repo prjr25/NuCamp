d8078d3b288a5dc37bd0c62245af169b7a9061f8
Patrick <prjr25@gmail.com>
2020-11-04 20:22:46 -0800

    Redux Thunk

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
         Label  } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
     const required = val => val && val.length;
     const minLength = len => val => val && (val.length >= len);
@@ -46,7 +47,27 @@ import { Link } from 'react-router-dom';
     }
 
     function CampsiteInfo(props) {
-        
+
+            if (props.isLoading) {
+                return (
+                    <div className="container">
+                        <div className="row">
+                            <Loading />
+                        </div>
+                    </div>
+                );
+            }
+            if (props.errMess) {
+                return (
+                    <div className="container">
+                        <div className="row">
+                            <div className="col">
+                                <h4>{props.errMess}</h4>
+                            </div>
+                        </div>
+                    </div>
+                );
+        }
         if (props.campsite) {
             return (
                 <div className="container">
